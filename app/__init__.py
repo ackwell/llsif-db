91830fee280782dec0c5e051ce558da12d998907
Saxon Landers <saxon@ackwell.com.au>
2014-06-27 13:55:17 +0000

    Will not render header/footer if requesting with ajax param

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
 
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config.from_object('app.config')
+
+@app.context_processor
+def context_processor():
+	context = {}
+
+	# Add an `ajax` var, true when page is requested with `ajax` field in GET or POST
+	context['ajax'] = False
+	if request.form.get('ajax', False) or request.args.get('ajax', False):
+		context['ajax'] = True
+
+	return context
 
 from . import controllers, models
