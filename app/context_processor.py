55e938e70f38666ea9184f9e444b7d4083cfca5a
Saxon Landers <saxon@ackwell.com.au>
2014-07-20 11:46:53 +0000

    Allowed static url overriding from config

--- app/context_processor.py
+++ app/context_processor.py
@@ -1,9 +1,11 @@
 
+from flask import current_app, url_for
 from flask.ext.security import current_user
 from .util import DeleteForm
 
 def context_processor():
 	return {
 		'logged_in': current_user.is_authenticated(),
-		'delete_form': DeleteForm
+		'delete_form': DeleteForm,
+		'static_files': current_app.config['STATIC_FILE_BASE'] if 'STATIC_FILE_BASE' in current_app.config else url_for('static', filename='')
 	}
